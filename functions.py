f86748d804fdea434ae3ce395f1db4d2095902b0
Pablo <122838544+pablorobba@users.noreply.github.com>
2023-10-30 00:29:39 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-df_developer = pd.read_csv(r"Api_DataFrame\developer.csv")
-df_userdata = pd.read_csv(r"Api_DataFrame\userdata.csv")
+df_developer = pd.read_csv(r"Api_DataFrame/developer.csv")
+df_userdata = pd.read_csv(r"Api_DataFrame/userdata.csv")
 df_best_developer_year= pd.read_csv(r"Api_DataFrame/best_developer_year.csv")
 df_UserForGenre_genre = pd.read_csv(r"Api_DataFrame/UserForGenre_genre.csv")
-df_UserForGenre_year = pd.read_csv(r"Api_DataFrame\UserForGenre_year.csv")
-df_developer_reviews_analysis = pd.read_csv(r"Api_DataFrame\developer_reviews_analysis.csv")
+df_UserForGenre_year = pd.read_csv(r"Api_DataFrame/UserForGenre_year.csv")
+df_developer_reviews_analysis = pd.read_csv(r"Api_DataFrame/developer_reviews_analysis.csv")
 
 def presentation():
     '''
